4cb60b9fa917265dce35742762547eda54cc0b90
Isma-Akin <aakinsemoyin@hotmail.com>
2023-02-13 16:38:17 +0000

    add listtile widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,67 @@ void main() {
   runApp(const Todo());
 }
 
-class Todo extends StatelessWidget {
+
+class Todo extends StatefulWidget {
   const Todo({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    // app layout
-    return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('To-do List', style: TextStyle(color: Colors.blue),),
-          ),
-          body: Container(alignment: Alignment.center, color: Colors.grey,
-            child: Row(children: [
-              Container(alignment: Alignment.center,)
-            ],) ,
-
-          ),
-        )
-    );
+  State<Todo> createState() => _TodoState();
 }
+
+class _TodoState extends State<Todo> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: const Text(
+              "Task list",
+              style: TextStyle(color: Colors.black, fontFamily: "Arial", fontSize: 30,),
+            ),
+            leading: IconButton(
+              tooltip: "Close",
+              icon: const Icon(Icons.close_rounded),
+              color: Colors.black,
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            centerTitle: true,
+          ),
+          body: Column(
+            children: [
+              Expanded(child: ListView(shrinkWrap: true,
+                children: const [
+                  SizedBox(
+                    height: 100,
+                    child: ListTile(
+                    leading: Icon(Icons.library_books_sharp),
+                    title: Text("Task 1"), tileColor: Colors.purple,
+                    subtitle: Text("Your first task"),),
+                  ),
+                  SizedBox(
+                    height: 100,
+                    child: ListTile(
+                    leading: Icon(Icons.library_books_sharp),
+                    title: Text("Task 2"), tileColor: Colors.blue,
+                        subtitle: Text("Your second task") ),
+                  ),
+                  ListTile(
+                  leading: Icon(Icons.library_books_sharp),
+                  title: Text("Task 3"), tileColor: Colors.green,
+                  subtitle: Text("Your third task"),),
+                  ListTile(
+                  leading: Icon(Icons.library_books_sharp),
+                  title: Text("Task 4"), tileColor: Colors.orange,
+                  subtitle: Text("Your fourth task"),),
+                ],
+              ))
+            ],
+          )
+
+      ),
+    );}
 }
