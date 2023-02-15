90c9f887b0607d10e17010cd773c8aebc5ee91b8
Isma-Akin <aakinsemoyin@hotmail.com>
2023-02-15 20:31:58 +0000

    refactor(blocs, todos_status_bloc): add imports; add class

---
+++ lib/blocs/todos_status/todos_status_bloc.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '/blocs/blocs.dart';
+
+import '/main_bloc_observer.dart';
+
+
+class TodosStatusBloc extends Bloc<TodosStatusEvent, TodosStatusState> {
+  final TodosBloc _todosBloc;
+  late StreamSubscription _todosSubscription;
+
+  TodosStatusBloc({required TodosBloc todosBloc})
+      : _todosBloc = todosBloc,
+        super(TodosStatusLoading()) {
+    on<UpdateTodosStatus>(_onUpdateTodosStatus);
+
+    _todosSubscription = _todosBloc.stream.listen((state) {
+      if (state is TodosLoaded) {
+        add(
+          UpdateTodosStatus(todos: state.todos),
+        );
+      }
+    });
+  }
+
+  void_onUpdateTodosStatus(
+    UpdateTodosStatus event,
+    Emitter<TodosStatusState> emit,
+  ) {
+    List<Todo> pendingTodos = event.todos
+        .where((todo) => todo.isCancelled == false && todo.isCompleted == false)
+        .toList();
+    List<Todo> completedTodos = event.todos
+        .where((todo) => todo.isCompleted == true && todo.isCancelled == false)
+        .toList();
+    List<Todo> cancelledTodos =
+        event.todos.where((todo) => todo.isCancelled == false).toList();
+
+    emit(
+      TodosStatusLoaded(
+        pendingTodos: pendingTodos,
+        completedTodos: completedTodos,
+        cancelledTodos: cancelledTodos,
+      ),
+    );
+  }
+
+  @override
+  Future<void> close() {
+    _todosSubscription.cancel();
+    return super.close();
+  }
+}
