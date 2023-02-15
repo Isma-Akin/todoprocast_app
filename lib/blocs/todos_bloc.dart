af4ae909aca90f54f14755837f54fbe38a24f4a1
Isma-Akin <aakinsemoyin@hotmail.com>
2023-02-15 15:45:56 +0000

    refactor: add import, class LoadTodos; AddTodo

--- lib/blocs/todos_bloc.dart
+++ lib/blocs/todos_bloc.dart
@@ -3,6 +3,9 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../main.dart';
+import '../models/todo_models.dart';
+
 part 'todos_event.dart';
 part 'todos_state.dart';
 
--- lib/blocs/todos_event.dart
+++ lib/blocs/todos_event.dart
@@ -2,4 +2,29 @@ part of 'todos_bloc.dart';
 
 abstract class TodosEvent extends Equatable {
   const TodosEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class LoadTodos extends TodosEvent {
+  final List<Todo> todos;
+
+  const LoadTodos({
+    this.todos = const <Todo>[],
+});
+
+  @override
+  List<Object> get props => [todos];
+}
+
+class AddTodo extends TodosEvent {
+  final Todo todo;
+
+  const AddTodo({
+    required this.todo,
+});
+
+  @override
+  List<Object> get props => [todo];
 }
