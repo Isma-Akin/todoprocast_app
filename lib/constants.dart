8234c8e9c77930908cd1ebe742297f5af25552c2
Isma-Akin <aakinsemoyin@hotmail.com>
2023-02-17 16:10:17 +0000

    refactor(constants): add constant and final

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const color = [Colors.blue, Colors.orange, Colors.green, Colors.purple];
+final taskdetails = [
+  "Your first task",
+  "Your second task",
+  "Your third task",
+  "Your fourth task"
+];
+
+final tasks = ["Task 1", "Task 2", "Task 3", "Task 4"];
