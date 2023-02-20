89ecce5639dcce117a2aaf3454244f036576a49f
Isma-Akin <aakinsemoyin@hotmail.com>
2023-02-20 15:22:46 +0000

    feat(todo_detail_screen): add detail screen for single todo

---
+++ lib/screens/todo_detail_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:todoprocast_app/models/todo_models.dart';
+
+class TodoDetailScreen extends StatelessWidget {
+  const TodoDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
