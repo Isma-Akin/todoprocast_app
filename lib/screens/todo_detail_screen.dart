3776fe7d1e5cc5c0262b6125bf3ccc4d48ee7fb2
Isma-Akin <aakinsemoyin@hotmail.com>
2023-02-26 20:08:35 +0000

    feat(todo_detail_screen): add switch toggle for completed task

--- lib/screens/todo_detail_screen.dart
+++ lib/screens/todo_detail_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todoprocast_app/models/todo_models.dart';
+
+import '../blocs/todos/todos_bloc.dart';
 
 class TodoDetailScreen extends StatelessWidget {
   final Todo todo;
@@ -10,7 +13,11 @@ class TodoDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Todo Details"),
+        title: Text('Todo Details: ${todo.task}',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 28,
+                color: Colors.white )),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -20,17 +27,32 @@ class TodoDetailScreen extends StatelessWidget {
             Text('Todo ID: ${todo.id}',
             style: Theme.of(context).textTheme.headline6,
             ),
-            const SizedBox(height: 8,),
-            Text('Task: ${todo.task}',
-            style: Theme.of(context).textTheme.subtitle1,
+            const Divider(height: 8,),
+            const SizedBox(height: 20,),
+            Row(mainAxisAlignment: MainAxisAlignment.end,children: [
+              Icon(Icons.home)
+            ],
             ),
-            const SizedBox(height: 8,
+            Text('Task: ${todo.task}',
+            style: Theme.of(context).textTheme.subtitle1
+            ),
+            const SizedBox(height: 20,
             ),
             Text('Description: ${todo.description}',
             style: Theme.of(context).textTheme.bodyText1,
             ),
-            const SizedBox(height: 8,
+            const SizedBox(height: 20,
             ),
+            Switch(value: todo.taskCompleted ?? false,
+                focusColor: Colors.blue,
+                inactiveThumbColor: Colors.grey,
+                activeColor: Colors.green,
+                onChanged: (newValue) {
+              context.read<TodosBloc>().add(
+                UpdateTodo(
+                todo: todo.copyWith(taskCompleted: newValue),
+              ),);
+                }),
             Text('Completed: ${'Yes or no'}',
             style: Theme.of(context).textTheme.subtitle2,)
           ],
