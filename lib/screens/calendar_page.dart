173246d4873a18c20515b90b807c0c40deb418ba
Isma-Akin <aakinsemoyin@hotmail.com>
2023-03-27 17:13:11 +0100

    feat(calendar_page): add due date tracker for todos and selectable days

--- lib/screens/calendar_page.dart
+++ lib/screens/calendar_page.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
@@ -7,6 +5,7 @@ import 'package:table_calendar/table_calendar.dart';
 import 'package:todoprocast_app/constants.dart';
 
 import '../blocs/todos/time_bloc.dart';
+import '../models/todo_models.dart';
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({Key? key}) : super(key: key);
@@ -31,7 +30,14 @@ class _CalendarPageState extends State<CalendarPage> {
   //   _timer?.cancel();
   //   super.dispose();
   // }
+  DateTime _selectedDay = DateTime.now();
 
+  List<Todo> _selectedTasks = [];
+  final List<Todo> _selectedTask = [
+    Todo(task: 'Task 1', description: 'Do task 1', dueDate: DateTime.now(), id: '', dateCreated: DateTime.now(),),
+    Todo(task: 'Task 1', description: 'Do task 1', dueDate: DateTime.now(), id: '', dateCreated: DateTime.now(),),
+    Todo(task: 'Task 1', description: 'Do task 1', dueDate: DateTime.now(), id: '', dateCreated: DateTime.now(),),
+  ];
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -60,6 +66,41 @@ class _CalendarPageState extends State<CalendarPage> {
                       firstDay: DateTime.utc(2010, 10, 16),
                       lastDay: DateTime.utc(2030, 3, 14),
                       focusedDay: DateTime.now(),
+                      selectedDayPredicate: (day) => isSameDay(day, _selectedDay),
+                      onDaySelected: (selectedDay, focusedDay) {
+                        setState(() {
+                          _selectedDay = selectedDay;
+                          _selectedTasks = _selectedTask.where((task) =>
+                              isSameDay(task.dueDate, selectedDay)).toList();
+                        });
+                        },
+                      calendarFormat: CalendarFormat.month,
+                      headerStyle: const HeaderStyle(
+                        formatButtonVisible: false,
+                        titleCentered: true,
+                      ),
+                      calendarStyle: const CalendarStyle(
+                        selectedDecoration: BoxDecoration(
+                          color: Colors.blue,
+                          shape: BoxShape.circle,
+                        ),
+                        todayDecoration: BoxDecoration(
+                          color: Colors.orange,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 300,
+                      child: ListView.builder(
+                        itemCount: _selectedTasks.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(_selectedTasks[index].task + " is due on this day!"),
+                            subtitle: Text(_selectedTasks[index].description),
+                          );
+                        },
+                      ),
                     ),
                   ]
               );
