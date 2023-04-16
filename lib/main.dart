f5ddb05aa850d92f8d4ebcf746a18c801f966cb8
Isma-Akin <aakinsemoyin@hotmail.com>
2023-04-16 21:53:58 +0100

    refactor(main): add hive imports, hive initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
 import 'package:todoprocast_app/main_bloc_observer.dart';
 import 'package:todoprocast_app/blocs/todos/todos_bloc.dart';
 import 'package:todoprocast_app/blocs/todos_status/todos_status_bloc.dart';
 import 'package:todoprocast_app/screens/main_screen.dart';
 
+import 'api/todo_repository.dart';
 import 'logic/navigation/navigation_cubit.dart';
+import 'models/todo_models.dart';
 
-void main() {
+Future<void> main() async {
+  //initialize hive
+  await Hive.initFlutter();
+  //register adapters for todo class
+  Hive.registerAdapter(TodoAdapter());
+  //open box for todos
+  await Hive.openBox<Todo>('todos');
+
+  // final todosRepository = TodoRepository(todoBox);
+  // final todosBloc = TodosBloc(todosRepository: todosRepository);
   Bloc.observer = MainBlocObserver();
   runApp(const TodoApp());
 }
@@ -45,38 +58,28 @@ class TodoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(providers:[
-      BlocProvider(
-      create: (context) => TodosBloc()
-      ..add(
-        const LoadTodos(
-          todos: [
-          //   Todo(
-          // id: 3,
-          // task: 'Your first task',
-          // description: 'Task description',
-          // dateCreated: DateTime.now(),
-          // dueDate: DateTime.now()),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<TodosBloc>(
+          create: (context) => TodosBloc(TodoRepository()),
+        ),
+        BlocProvider<TodosStatusBloc>(
+          create: (context) => TodosStatusBloc(
+            todosBloc: BlocProvider.of<TodosBloc>(context),
+          )..add(const UpdateTodosStatus()),
+        ),
+        BlocProvider<NavigationCubit>(
+          create: (context) => NavigationCubit(),
+        ),
       ],
-     ),
-    ),
-   ),
-    BlocProvider(
-      create: (context) => TodosStatusBloc(
-        todosBloc: BlocProvider.of<TodosBloc>(context),
-      )..add(const UpdateTodosStatus()),
-    ),
-    BlocProvider<NavigationCubit>(
-    create: (context) => NavigationCubit(),
-    ),
-    ],
-        child: MaterialApp(
-          title: 'Todo App',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: const MainScreen(),
-        ));
+      child: MaterialApp(
+        title: 'Todo App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MainScreen(),
+      ),
+    );
   }
 }
