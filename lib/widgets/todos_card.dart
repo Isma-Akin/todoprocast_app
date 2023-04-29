4041772b41099fdde047ba894b2db5ee3964b1b8
Isma-Akin <aakinsemoyin@hotmail.com>
2023-04-29 21:31:38 +0100

    refactor(todos_card): import likebutton; replace iconbutton with likebutton

--- lib/widgets/todos_card.dart
+++ lib/widgets/todos_card.dart
@@ -5,6 +5,7 @@ import '../blocs/todos/todos_bloc.dart';
 import '../constants.dart';
 import '../models/todo_models.dart';
 import '../screens/todo_detail_screen.dart';
+import 'package:like_button/like_button.dart';
 
 
 InkWell todosCard(
@@ -33,10 +34,11 @@ InkWell todosCard(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              '#${todo.id}: ${todo.task}',
+              '${todo.id}: ${todo.task}',
               style: const TextStyle(
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
+                color: Colors.black,
               ),
             ),
             const Divider(
@@ -46,6 +48,7 @@ InkWell todosCard(
               style: const TextStyle(
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
             ),
             BlocBuilder<TodosBloc, TodosState>(
@@ -53,14 +56,30 @@ InkWell todosCard(
                 return Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    IconButton(
-                      onPressed: () {
+                    LikeButton(
+                      isLiked: todo.isFavourite,
+                      onTap: (isLiked) {
                         context.read<TodosBloc>().add(
-                            MarkTodoAsFavOrUnFav(todo: todo));
+                          MarkTodoAsFavOrUnFav(todo: todo),
+                        );
+                        return Future.value(!isLiked);
                       },
-                      icon: Icon(
-                          todo.isFavourite! ? Icons.star : Icons.star_border),
-                      color: Colors.yellow,
+                      size: 25,
+                      circleColor: CircleColor(
+                        start: Color(0xff00ddff),
+                        end: Color(0xff0099cc),
+                      ),
+                      bubblesColor: BubblesColor(
+                        dotPrimaryColor: Color(0xff33b5e5),
+                        dotSecondaryColor: Color(0xff0099cc),
+                      ),
+                      likeBuilder: (bool isLiked) {
+                        return Icon(
+                          isLiked ? Icons.star : Icons.star_border,
+                          color: Colors.yellow,
+                          size: 25,
+                        );
+                      },
                     ),
                     InkWell(
                       onTap: () {
