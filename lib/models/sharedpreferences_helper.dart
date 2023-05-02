a8dc7694e0dd5bf9493729df21a78f578a0a3f11
Isma-Akin <aakinsemoyin@hotmail.com>
2023-05-02 22:20:28 +0100

    add sharedpreferences_helper.dart

---
+++ lib/models/sharedpreferences_helper.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todoprocast_app/models/todo_models.dart';
+
+class SharedPreferencesHelper {
+  static const String _kTodosKey = 'todos';
+
+  static Future<List<Todo>> getTodos() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final todosJson = prefs.getString(_kTodosKey);
+    if (todosJson == null) {
+      return [];
+    }
+    final List<dynamic> todoListJson = json.decode(todosJson);
+    return todoListJson.map((json) => Todo.fromJson(json)).toList();
+  }
+
+  static Future<bool> saveTodos(List<Todo> todos) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final todosJson = json.encode(todos.map((todo) => todo.toJson()).toList());
+    return prefs.setString(_kTodosKey, todosJson);
+  }
+
+  static Future<bool> saveTodoToLocal(Todo todo) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final List<Todo> todos = await getTodos();
+    final index = todos.indexWhere((t) => t.id == todo.id);
+    if (index != -1) {
+      todos[index] = todo;
+    } else {
+      todos.add(todo);
+    }
+    return saveTodos(todos);
+  }
+
+  static Future<bool> removeTodoFromLocal(Todo todo) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final List<Todo> todos = await getTodos();
+    final index = todos.indexWhere((t) => t.id == todo.id);
+    if (index != -1) {
+      todos.removeAt(index);
+      return saveTodos(todos);
+    }
+    return false;
+  }
+
+  static Future<bool> deleteTodoFromLocal(int id) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final todosJson = prefs.getString(_kTodosKey);
+    if (todosJson == null) {
+      return false;
+    }
+    final List<dynamic> todoListJson = json.decode(todosJson);
+    final List<Todo> todos =
+    todoListJson.map((json) => Todo.fromJson(json)).toList();
+    final int index =
+    todos.indexWhere((todo) => todo.id != null && todo.id == id);
+    if (index == -1) {
+      return false;
+    }
+    todos.removeAt(index);
+    final updatedTodosJson =
+    json.encode(todos.map((todo) => todo.toJson()).toList());
+    return prefs.setString(_kTodosKey, updatedTodosJson);
+  }
+
+  static Future<bool> deleteAllTodosFromLocal() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return await prefs.remove(_kTodosKey);
+  }
+}
+
+
