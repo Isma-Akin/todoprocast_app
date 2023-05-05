769f23450e530d98e55a51f5c9a339d54e238897
Isma-Akin <aakinsemoyin@hotmail.com>
2023-05-05 23:50:57 +0100

    refactor(task_planner_screen): add sliverappbar; init and dispose methods; modify animated container padding

--- lib/screens/task_planner_screen.dart
+++ lib/screens/task_planner_screen.dart
@@ -17,10 +17,10 @@ class TaskPlanner extends StatefulWidget {
   State<TaskPlanner> createState() => _TaskPlannerState();
 }
 
-class _TaskPlannerState extends State<TaskPlanner> {
+class _TaskPlannerState extends State<TaskPlanner> with TickerProviderStateMixin {
   bool _isCompletedListVisible = true;
   bool _isPendingListVisible = true;
-
+  late TabController _tabController;
   void _addTodo(BuildContext context) {
     showModalBottomSheet(
         context: context,
@@ -32,6 +32,44 @@ class _TaskPlannerState extends State<TaskPlanner> {
             child: AddTodoScreen(todosBloc: context.read<TodosBloc>()),
       ),
     ));
+  }
+
+  void _showDeleteAllTodosDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Delete Todo'),
+          content: const Text('Are you sure you want to delete every todo?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('CANCEL'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                context.read<TodosBloc>().add(RemoveAllTodos());
+              },
+              child: const Text('DELETE'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 1, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
@@ -51,119 +89,108 @@ class _TaskPlannerState extends State<TaskPlanner> {
           },child: const Icon(Icons.add, size: 30),
             elevation: 0.1,
         ),
-            appBar: AppBar(
-                backgroundColor: Colors.orange,
-              elevation: 0,
-              actions: [
-                IconButton(onPressed: () {
-                  PopupMenuButton(
-                    itemBuilder: (BuildContext context) => <PopupMenuEntry>[
-                      PopupMenuItem(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const Settings(),
-                            ),
-                          );
-                        },
-                        value: 1,
-                        child: const Text('Settings'),
-                      ),
-                      PopupMenuItem(
-                        value: 2,
-                        child: Text('Change Theme'),
-                      ),
-                      PopupMenuItem(
-                        value: 3,
-                        child: Text('View Profile'),
-                      ),
-                    ],
-                    onSelected: (value) {
-                      // Handle menu item selection here
-                    },
-                    icon: Icon(Icons.more_vert),
-                  );
-                },
-                  icon: const Icon(Icons.table_rows_rounded, color: AppColors.blueTertiaryColor,)),
-              ],
-              title: Center(
-                child: Text("Task planner",
-                style: GoogleFonts.openSans(
-                    color: Colors.black,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold),),
-              ),
+          body: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                flexibleSpace: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.orange, Colors.orangeAccent],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                  ),
+                ),
               leading: IconButton(
                 onPressed: () {
                   Navigator.pop(context);
                 },
-                icon: const Icon(Icons.arrow_back, color: AppColors.blueTertiaryColor,),
+                icon: const Icon(
+                  Icons.navigate_before,
+                  color: AppColors.blueTertiaryColor,
+                ),
               ),
-            ),
-          body: SafeArea(
-            top: true,
-            child: BlocBuilder<TodosStatusBloc, TodosStatusState>(
-              builder: (context, state) {
-                BlocProvider.of<TodosBloc>(context).add(const LoadTodos());
-                print('Current TodosStatusState: $state');
-                if (state is TodosStatusLoading) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                if (state is TodosStatusLoaded) {
-                  return CustomScrollView(
-                    shrinkWrap: true,
-                    physics: const BouncingScrollPhysics(),
-                    slivers: [
-                      SliverPadding(
-                        padding: const EdgeInsets.all(8.0),
-                        sliver: SliverList(
-                          delegate: SliverChildListDelegate([
-                            _todoPending(
-                              state.pendingTodos,
-                              'Pending',
-                            ),
-                            if (state.pendingTodos.isEmpty)
-                              Column(
-                                children: const [
-                              Text('You have no pending tasks.',
-                                    style: TextStyle(
-                                      color: AppColors.blueSecondaryColor,
-                                      fontSize: 26,
-                                      fontWeight: FontWeight.bold),),
-                              SizedBox(height: 20,),
-                              Text('Add a task by clicking the + button below.',
-                                      style: TextStyle(
-                                      color: AppColors.blueSecondaryColor,
-                                      fontSize: 20.2,
-                                      fontWeight: FontWeight.bold),),
-                                ],
-                              ),
-                              _todoCompleted(
-                              state.completedTodos,
-                              'Completed',
-                              ),
-                              // if (state.completedTodos.isEmpty)
-                              //  const Text('You have no completed tasks.',
-                              //     style: TextStyle(
-                              //     color: AppColors.blueSecondaryColor,
-                              //     fontSize: 27,
-                              //     fontWeight: FontWeight.bold),),
-                                  ]),
-                                ),
-                              ),
-                            ],
-                          );
-                        } else {
-                          return const Text('Something went wrong.');
-                        }
-                      },
-                    ),
+              title:  Center(
+                  child: Text('Task planner', style: GoogleFonts.openSans(fontSize: 25, color: Colors.black),)),
+                actions: [
+                  IconButton(
+                    icon: const Icon(Icons.settings,color: AppColors.blueTertiaryColor,),
+                    onPressed: () {
+                      // Code to open settings
+                    },
                   ),
-                ));
-          }
+                ],
+                floating: true,
+                snap: true,
+              ),
+              SliverToBoxAdapter(
+                child: SafeArea(
+                  top: true,
+                  child: BlocBuilder<TodosStatusBloc, TodosStatusState>(
+                    builder: (context, state) {
+                      BlocProvider.of<TodosBloc>(context).add(const LoadTodos());
+                      print('Current TodosStatusState: $state');
+                      if (state is TodosStatusLoading) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      if (state is TodosStatusLoaded) {
+                        return CustomScrollView(
+                          shrinkWrap: true,
+                          physics: const BouncingScrollPhysics(),
+                          slivers: [
+                            SliverPadding(
+                              padding: const EdgeInsets.all(1.0),
+                              sliver: SliverList(
+                                delegate: SliverChildListDelegate([
+                                  _todoPending(
+                                    state.pendingTodos,
+                                    'Active',
+                                  ),
+                                  if (state.pendingTodos.isEmpty)
+                                    Column(
+                                      children: const [
+                                        Text('You have no pending tasks.',
+                                          style: TextStyle(
+                                              color: AppColors.blueSecondaryColor,
+                                              fontSize: 26,
+                                              fontWeight: FontWeight.bold),),
+                                        SizedBox(height: 20,),
+                                        Text('Add a task by clicking the + button below.',
+                                          style: TextStyle(
+                                              color: AppColors.blueSecondaryColor,
+                                              fontSize: 20.2,
+                                              fontWeight: FontWeight.bold),),
+                                      ],
+                                    ),
+                                  _todoCompleted(
+                                    state.completedTodos,
+                                    'Completed',
+                                  ),
+                                  // if (state.completedTodos.isEmpty)
+                                  //  const Text('You have no completed tasks.',
+                                  //     style: TextStyle(
+                                  //     color: AppColors.blueSecondaryColor,
+                                  //     fontSize: 27,
+                                  //     fontWeight: FontWeight.bold),),
+                                ]),
+                              ),
+                            ),
+                          ],
+                        );
+                      } else {
+                        return const Text('Something went wrong.');
+                      }
+                    },
+                  ),
+                ),
+              ),
+              ],
+            ),
+          ),
+        );
+  }
 
 
 Column _todoPending(List<Todo> todos, String status) {
@@ -171,49 +198,76 @@ Column _todoPending(List<Todo> todos, String status) {
     children: [
       SizedBox(
         height: 50,
-        child: Row(
-          children: [
-            Text(
-              '$status Tasks: ${todos.length}',
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.orange[100],
+            borderRadius: BorderRadius.circular(2),
+            border: Border.all(color: Colors.black),
+          ),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(
+                  '$status Tasks: ${todos.length}',
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            ),
-            const Spacer(),
-            Visibility(
-              visible: todos.isNotEmpty,
-              child: IconButton(
+              const Spacer(),
+              Visibility(
+                visible: todos.isNotEmpty,
+                child: IconButton(
+                    onPressed: () {
+                      _showDeleteAllTodosDialog(context);
+                    },
+                    icon: const Icon(Icons.delete_forever, color: Colors.grey,)),
+              ),
+              Visibility(
+                visible: todos.isNotEmpty,
+                child: IconButton(
                   onPressed: () {
-                    context.read<TodosBloc>().add(RemoveAllTodos());
+                    setState(() {
+                      _isPendingListVisible = !_isPendingListVisible;
+                    });
                   },
-                  icon: const Icon(Icons.delete_forever)),
-            ),
-            Visibility(
-              visible: todos.isNotEmpty,
-              child: IconButton(
-                onPressed: () {
-                  setState(() {
-                    _isPendingListVisible = !_isPendingListVisible;
-                  });
-                },
-                icon: Icon(_isPendingListVisible ? Icons.visibility : Icons.visibility_off),
+                  icon: Icon(_isPendingListVisible ? Icons.visibility : Icons.visibility_off, color: Colors.grey,),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-      if (_isPendingListVisible)
-        ListView.builder(
+      // if (_isPendingListVisible)
+      //   ListView.builder(
+      //     shrinkWrap: true,
+      //     itemCount: todos.length,
+      //     itemBuilder: (BuildContext context, int index) {
+      //       return todosCard(
+      //         context,
+      //         todos[index],
+      //       );
+      //     },
+      //   ),
+      AnimatedContainer(
+        height: _isPendingListVisible ? todos.length * 104 : 0.0,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+        child: ListView.builder(
           shrinkWrap: true,
           itemCount: todos.length,
           itemBuilder: (BuildContext context, int index) {
-            return todosCard(
-              context,
-              todos[index],
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 2.0, left: 3.0, right: 3.0, top: 8.0),
+              child: todosCard(
+                context, todos[index],
+              ),
             );
           },
         ),
+      ),
     ],
   );
 }
@@ -221,65 +275,79 @@ Column _todoPending(List<Todo> todos, String status) {
 Column _todoCompleted(List<Todo> todos, String status) {
   return Column(
     children: [
-      Divider(
+      const Divider(
         thickness: 2,
         color: Colors.grey,
       ),
       SizedBox(
         height: 50,
-        child: Row(
-          children: [
-            Text(
-              '$status Tasks: ${todos.length}',
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.orange[100],
+            borderRadius: BorderRadius.circular(2),
+            border: Border.all(color: Colors.black),
+          ),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(
+                  '$status Tasks: ${todos.length}',
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            ),
-            const Spacer(),
-            Visibility(
-              visible: todos.isNotEmpty,
-              child: IconButton(
-                onPressed: () {
-                  context.read<TodosBloc>().add(RemoveAllTodos());
-                },
-                icon: const Icon(Icons.delete_forever),
+              const Spacer(),
+              Visibility(
+                visible: todos.isNotEmpty,
+                child: IconButton(
+                    onPressed: () {
+                      _showDeleteAllTodosDialog(context);
+                    },
+                    icon: const Icon(Icons.delete_forever, color: Colors.grey,)),
               ),
-            ),
-            Visibility(
-              visible: todos.isNotEmpty,
-              child: IconButton(
-                onPressed: () {
-                  setState(() {
-                    _isCompletedListVisible = !_isCompletedListVisible;
-                  });
-                },
-                icon: Icon(_isCompletedListVisible ? Icons.visibility : Icons.visibility_off),
+              Visibility(
+                visible: todos.isNotEmpty,
+                child: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _isCompletedListVisible = !_isCompletedListVisible;
+                    });
+                  },
+                  icon: Icon(_isCompletedListVisible ? Icons.visibility : Icons.visibility_off, color: Colors.grey,),
+                ),
               ),
-            ),
-            // IconButton(
-            //   onPressed: () {
-            //     setState(() {
-            //       _isListVisible = !_isListVisible;
-            //     });
-            //   },
-            //   icon: Icon(_isListVisible ? Icons.visibility : Icons.visibility_off),
-            // ),
-          ],
+              // IconButton(
+              //   onPressed: () {
+              //     setState(() {
+              //       _isListVisible = !_isListVisible;
+              //     });
+              //   },
+              //   icon: Icon(_isListVisible ? Icons.visibility : Icons.visibility_off),
+              // ),
+            ],
+          ),
         ),
       ),
-      if (_isCompletedListVisible)
-ListView.builder(
-        shrinkWrap: true,
-        itemCount: todos.length,
-        itemBuilder: (BuildContext context, int index) {
-          return todosCard(
-          context,
-          todos[index],
-         );
+      AnimatedContainer(
+        height: _isCompletedListVisible ? todos.length * 104 : 0.0,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+        child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: todos.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 2.0, left: 3.0, right: 3.0, top: 8.0),
+              child: todosCard(
+                context, todos[index],
+         ),
+            );
         },
+      ),
      ),
-
     ],
   );
 }
