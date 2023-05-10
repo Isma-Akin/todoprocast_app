585449669408ad439f9a363062610d7d3c5c1aca
Isma-Akin <aakinsemoyin@hotmail.com>
2023-05-10 22:27:46 +0100

    refactor(todo_repository): remove hive implementation; add shared preferences

--- lib/api/todo_repository.dart
+++ lib/api/todo_repository.dart
@@ -1,43 +1,41 @@
 import 'dart:convert';
 
-import 'package:hive/hive.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
+
+import '../models/sharedpreferences_helper.dart';
 import '../models/todo_models.dart';
 
 class TodoRepository {
+  final SharedPreferences _prefs;
+  TodoRepository({required SharedPreferences prefs}) : _prefs = prefs;
+
   static const String baseUrl = 'http://localhost:8080';
   static const String createTodoUrl = '/todos';
   static const String getAllTodosUrl = '/todos';
 
   static Future<List<Todo>> getAllTodos() async {
-    // First, check if there are todos in the local storage
-    final Box<Todo> todosBox = await Hive.openBox<Todo>('todos');
-    final List<Todo> todos = todosBox.values.toList();
+    try {
+      final response = await http.get(Uri.parse(baseUrl + getAllTodosUrl));
 
-    // If there are todos in the local storage, return them
-    if (todos.isNotEmpty) {
+      if (response.statusCode == 200) {
+        final jsonResponse = jsonDecode(response.body);
+        final todos = jsonResponse.map<Todo>((json) => Todo.fromJson(json)).toList();
+        await SharedPreferencesHelper.saveTodos(todos);
+        return todos;
+      } else {
+        throw Exception('Failed to get todos.');
+      }
+    } catch (e) {
+      print('Error: $e');
+      final todos = await SharedPreferencesHelper.getTodos();
       return todos;
-    }
-
-    // If there are no todos in the local storage, fetch from the backend
-    final response = await http.get(Uri.parse(baseUrl + getAllTodosUrl));
-
-    if (response.statusCode == 200) {
-      final jsonResponse = jsonDecode(response.body);
-      final todos = jsonResponse.map<Todo>((json) => Todo.fromJson(json)).toList();
-
-      // Add fetched todos to the local storage
-      todosBox.addAll(todos);
-
-      return todos;
-    } else {
-      throw Exception('Failed to get todos.');
     }
   }
 
+
   static Future<Todo> createTodo(Todo todo) async {
-    // Send todo to the backend if there is an internet connection
     try {
       final response = await http.post(
         Uri.parse(baseUrl + createTodoUrl),
@@ -46,35 +44,45 @@ class TodoRepository {
           'task': todo.task,
           'description': todo.description,
           'dateCreated': todo.dateCreated.toIso8601String(),
-          'dueDate': todo.dueDate.toIso8601String(),
+          'dueDate': todo.dueDate?.toIso8601String(),
           'taskCompleted': todo.taskCompleted,
           'taskCancelled': todo.taskCancelled,
           'isFavourite': todo.isFavourite,
+          'steps': todo.steps,
         }),
       );
       if (response.statusCode == 201) {
         final dynamic todoJson = json.decode(response.body);
-        final Todo todo = Todo.fromJson(todoJson);
-        return todo;
+        final createdTodo = Todo.fromJson(todoJson);
+        final todos = await SharedPreferencesHelper.getTodos();
+        todos.add(createdTodo);
+        await SharedPreferencesHelper.saveTodos(todos);
+        return createdTodo;
       } else {
+        final todos = await SharedPreferencesHelper.getTodos();
+        todos.add(todo);
+        await SharedPreferencesHelper.saveTodos(todos);
         throw Exception('Failed to create todo: ${response.statusCode}');
       }
-    } catch (error) {
-      // If there is no internet connection, save the todo with an unsynced flag
-      todo.isSynced = false;
-      final Box<Todo> todosBox = await Hive.openBox<Todo>('todos');
-      final int id = await todosBox.add(todo);
-      return todo;
+    } catch (e) {
+      final todos = await SharedPreferencesHelper.getTodos();
+      todos.add(todo);
+      await SharedPreferencesHelper.saveTodos(todos);
+      rethrow;
     }
   }
 
+  // static Future<List<Todo>> searchTodos(String query) async {
+  //   final todos = await getAllTodos();
+  //   final matchingTodos = todos
+  //       .where((todo) =>
+  //       todo.task.toLowerCase().contains(query.toLowerCase()))
+  //       .toList();
+  //   return matchingTodos;
+  // }
+
 
   static Future<Todo> updateTodo(Todo todo) async {
-    // Update todo in local storage
-    final Box<Todo> todosBox = await Hive.openBox<Todo>('todos');
-    await todosBox.put(todo.id, todo);
-
-    // Update todo in backend if there is an internet connection
     try {
       final response = await http.put(
         Uri.parse(baseUrl + '/todos/${todo.id}'),
@@ -83,77 +91,57 @@ class TodoRepository {
           'task': todo.task,
           'description': todo.description,
           'dateCreated': todo.dateCreated.toIso8601String(),
-          'dueDate': todo.dueDate.toIso8601String(),
+          'dueDate': todo.dueDate?.toIso8601String(),
           'taskCompleted': todo.taskCompleted,
           'taskCancelled': todo.taskCancelled,
           'isFavourite': todo.isFavourite,
           'id': todo.id,
+          'steps': todo.steps,
         }),
       );
       if (response.statusCode == 200) {
         final jsonResponse = jsonDecode(response.body);
-        return Todo.fromJson(jsonResponse);
+        final updatedTodo = Todo.fromJson(jsonResponse);
+        await SharedPreferencesHelper.saveTodoToLocal(updatedTodo);
+        return updatedTodo;
       } else {
-        throw Exception('Failed to update todo.');
+        print('Failed to update todo on the backend');
       }
-    } catch (error) {
-      print('Failed to update todo on backend: $error');
+    } catch (e) {
+      print('Error updating todo on the backend: $e');
     }
-
-    // Return the updated todo
+    // If updating on the backend failed or encountered an error,
+    // update the local storage instead.
+    await SharedPreferencesHelper.saveTodoToLocal(todo);
     return todo;
   }
 
   static Future<void> deleteTodoById(int id) async {
     try {
       final response = await http.delete(Uri.parse(baseUrl + '/todos/$id'));
-
       if (response.statusCode != 204) {
-        throw Exception('Failed to delete todo.');
+        throw Exception('Failed to delete todo on the backend');
       }
-
-      final Box<Todo> todosBox = await Hive.openBox<Todo>('todos');
-      await todosBox.delete(id);
     } catch (error) {
-      final Box<Todo> todosBox = await Hive.openBox<Todo>('todos');
-      await todosBox.put(id, Todo(
-        id: id,
-        isSynced: false,
-        isTempId: false,
-        task: '',
-        description: '',
-        dateCreated: DateTime.now(),
-        dueDate: DateTime.now(),
-        taskCompleted: false,
-        taskCancelled: false,
-        isFavourite: false,
-        groupId: '',
-        steps: [],
-      ));
+      print('Failed to connect to the backend. Deleting todo locally.');
     }
+    await SharedPreferencesHelper.deleteTodoFromLocal(id);
   }
 
   static Future<void> deleteAllTodos() async {
     try {
       final response = await http.delete(Uri.parse(baseUrl + '/todos'));
 
-      if (response.statusCode != 204) {
-        throw Exception('Failed to delete all todos.');
+      if (response.statusCode == 204) {
+        await SharedPreferencesHelper.deleteAllTodosFromLocal();
+      } else {
+        print('Failed to delete all todos on the backend, attempting to delete locally');
+        await SharedPreferencesHelper.deleteAllTodosFromLocal();
       }
-
-      final Box<Todo> todosBox = await Hive.openBox<Todo>('todos');
-      await todosBox.clear();
-    } catch (error) {
-      // Handle error if the backend is down
-      final Box<Todo> todosBox = await Hive.openBox<Todo>('todos');
-      for (int i = 0; i < todosBox.length; i++) {
-        final todo = todosBox.getAt(i);
-        if (todo != null) {
-          todo.isSynced = false;
-          await todosBox.put(todo.id, todo);
-        }
-      }
+    } catch (e) {
+      print('Error deleting all todos on the backend: ${e.toString()}');
+      await SharedPreferencesHelper.deleteAllTodosFromLocal();
     }
   }
-}
 
+}
