c531ab8cd8c4ccb86e694b248b3ef063f525be24
Isma-Akin <aakinsemoyin@hotmail.com>
2023-05-16 18:08:57 +0100

    refactor(pomodoro_bloc): add new events and methods; refactor time cancel

--- lib/blocs/task_activities/pomodoro_bloc.dart
+++ lib/blocs/task_activities/pomodoro_bloc.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../models/todo_models.dart';
+
 part 'pomodoro_event.dart';
 part 'pomodoro_state.dart';
 
@@ -15,14 +17,17 @@ class PomodoroBloc extends Bloc<PomodoroEvent, PomodoroState> {
     on<ResetPomodoro>(_mapResetPomodoroToState);
     on<StopPomodoro>(_mapStopPomodoroToState);
     on<TickPomodoro>(_mapTickPomodoroToState);
+    on<PomodoroCompleted>(_mapPomodoroCompletedToState);
+    on<TickBreak>(_mapTickBreakToState);
+
   }
 
   void _mapStartPomodoroToState(StartPomodoro event, Emitter<PomodoroState> emit) {
-    emit(PomodoroRunning(1500));
     _timer?.cancel();
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       add(TickPomodoro());
     });
+    emit(PomodoroRunning(25 * 60));
   }
 
   void _mapPausePomodoroToState(PausePomodoro event, Emitter<PomodoroState> emit) {
@@ -30,20 +35,6 @@ class PomodoroBloc extends Bloc<PomodoroEvent, PomodoroState> {
       final secondsRemaining = (state as PomodoroRunning).secondsRemaining;
       _timer?.cancel();
       emit(PomodoroPaused(secondsRemaining));
-    } else if (state is PomodoroPaused) {
-      final secondsRemaining = (state as PomodoroPaused).secondsRemaining;
-      emit(PomodoroRunning(secondsRemaining));
-      _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-        if(state is PomodoroRunning) {
-          final remaining = (state as PomodoroRunning).secondsRemaining;
-          if (remaining > 0) {
-            emit(PomodoroRunning(remaining - 1));
-          } else {
-            _timer?.cancel();
-            emit(PomodoroStopped());
-          }
-        }
-      });
     }
   }
 
@@ -57,7 +48,6 @@ class PomodoroBloc extends Bloc<PomodoroEvent, PomodoroState> {
     }
   }
 
-
   void _mapTickPomodoroToState(TickPomodoro event, Emitter<PomodoroState> emit) {
     if (state is PomodoroRunning) {
       final secondsRemaining = (state as PomodoroRunning).secondsRemaining - 1;
@@ -65,7 +55,7 @@ class PomodoroBloc extends Bloc<PomodoroEvent, PomodoroState> {
         emit(PomodoroRunning(secondsRemaining));
       } else {
         _timer?.cancel();
-        emit(PomodoroStopped());
+        add(PomodoroCompleted());
       }
     }
   }
@@ -75,8 +65,41 @@ class PomodoroBloc extends Bloc<PomodoroEvent, PomodoroState> {
     emit(PomodoroRunning(25 * 60));
   }
 
+  void _mapPomodoroCompletedToState(PomodoroCompleted event, Emitter<PomodoroState> emit) {
+    emit(Break(5 * 60)); // let's say the break is 5 minutes
+    _timer = Timer.periodic(const Duration(seconds: 1), (_) {
+      if(state is Break) {
+        final remaining = (state as Break).secondsRemaining;
+        if (remaining > 0) {
+          emit(Break(remaining - 1));
+        } else {
+          _timer?.cancel();
+          emit(PomodoroInitial());
+        }
+      }
+    });
+  }
+
+  void _mapTickBreakToState(TickBreak event, Emitter<PomodoroState> emit) {
+    if (state is Break) {
+      final secondsRemaining = (state as Break).secondsRemaining - 1;
+      if (secondsRemaining > 0) {
+        emit(Break(secondsRemaining));
+      } else {
+        _timer?.cancel();
+        emit(PomodoroInitial());
+      }
+    }
+  }
+
   void _mapStopPomodoroToState(StopPomodoro event, Emitter<PomodoroState> emit) {
     _timer?.cancel();
-    emit(PomodoroInitial());
+    emit(PomodoroStopped());
+  }
+
+  @override
+  Future<void> close() {
+    _timer?.cancel();
+    return super.close();
   }
 }
