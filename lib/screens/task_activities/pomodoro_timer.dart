5e5adfe4ac1f8c68ad2160ed78cd369a3a81de4d
Isma-Akin <aakinsemoyin@hotmail.com>
2023-05-16 18:11:55 +0100

    refactor(pomodoro_timer): implement blocbuilder for todos and pomodoro; change to stateful widget; refactor states to factor in new events; add appbar background

--- lib/screens/task_activities/pomodoro_timer.dart
+++ lib/screens/task_activities/pomodoro_timer.dart
@@ -1,92 +1,352 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:todoprocast_app/blocs/todos_status/todos_status_bloc.dart';
 
 import '../../blocs/task_activities/pomodoro_bloc.dart';
+import '../../blocs/todos/todos_bloc.dart';
 import '../../models/todo_models.dart';
 
 
-class PomodoroTimer extends StatelessWidget {
-  final List<Todo> todos;
-  const PomodoroTimer({Key? key, required this.todos}) : super(key: key);
+class PomodoroTimer extends StatefulWidget {
+
+  const PomodoroTimer({Key? key,}) : super(key: key);
+
+  @override
+  State<PomodoroTimer> createState() => _PomodoroTimerState();
+}
+
+class _PomodoroTimerState extends State<PomodoroTimer> {
+  PomodoroBloc? _pomodoroBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _pomodoroBloc = BlocProvider.of<PomodoroBloc>(context);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color(0xFF00BFA5),
-                Color(0xFF00C853),
-              ],
-            ),
-          ),
-        ),
-        title: const Text('Pomodoro Timer'),
-      ),
-      body: BlocBuilder<PomodoroBloc, PomodoroState>(
+    _pomodoroBloc = BlocProvider.of<PomodoroBloc>(context);
+    return BlocBuilder<PomodoroBloc, PomodoroState>(
         builder: (context, state) {
-          if (state is PomodoroRunning) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '${(state.secondsRemaining / 60).floor()}:${(state.secondsRemaining % 60).toString().padLeft(2, '0')}',
-                  style: Theme.of(context).textTheme.headline1,
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () => context.read<PomodoroBloc>().add(PausePomodoro()),
-                  child: const Text('Pause'),
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () => context.read<PomodoroBloc>().add(ResetPomodoro()),
-                  child: const Text('Reset'),
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () => context.read<PomodoroBloc>().add(StopPomodoro()),
-                  child: const Text('Stop'),
-                ),
-              ],
-            );
-          } else if (state is PomodoroPaused) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '${(state.secondsRemaining / 60).floor()}:${(state.secondsRemaining % 60).toString().padLeft(2, '0')}',
-                  style: Theme.of(context).textTheme.headline1,
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () => context.read<PomodoroBloc>().add(ResumePomodoro()),
-                  child: const Text('Resume'),
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () => context.read<PomodoroBloc>().add(ResetPomodoro()),
-                  child: const Text('Reset'),
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () => context.read<PomodoroBloc>().add(StopPomodoro()),
-                  child: const Text('Stop'),
-                ),
-              ],
-            );
-          } else {
-            return Center(
-              child: ElevatedButton(
-                onPressed: () => context.read<PomodoroBloc>().add(StartPomodoro()),
-                child: const Text('Start'),
+          return Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: Text('Pomodoro Timer',
+                style: GoogleFonts.openSans(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold),),
+              flexibleSpace: FlexibleSpaceBar(
+                  background: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      Image.asset(
+                        'assets/images/cloudbackground.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                      Container(
+                        color: Colors.orange[900]?.withOpacity(0.6),
+                      ),
+                    ],
+                  )
               ),
-            );
-          }
+            ),
+            body: Center(
+              child: PomodoroTimerItem(),
+            ),
+          );
         },
-      ),
+     );
+  }
+}
+
+
+class PomodoroTimerItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<PomodoroBloc, PomodoroState>(
+      builder: (context, pomodoroState) {
+        print('Pomodoro state: $pomodoroState');
+        if (pomodoroState is PomodoroNotStarted ||
+            pomodoroState is PomodoroInitial) {
+          return pomodoroBlocBuilder();
+        } else if (pomodoroState is PomodoroRunning) {
+          return pomodoroRunning(pomodoroState, context);
+        } else if (pomodoroState is PomodoroPaused) {
+          return pomodoroPaused(pomodoroState, context);
+        } else if (pomodoroState is Break) {
+          return pomodoroBreak(context);
+        } else if (pomodoroState is PomodoroCompleted) {
+          return pomodoroCompleted(context);
+        } else {
+          return BlocBuilder<TodosStatusBloc, TodosStatusState>(
+            builder: (context, todosState) {
+              print('Todos state: $todosState');
+              if (todosState is TodosStatusLoaded) {
+                print('Pending todos count: ${todosState.pendingTodos.length}');
+                return Column(
+                  children: [
+                    DropdownButton(
+                      items: todosState.pendingTodos.map((Todo todo) {
+                        return DropdownMenuItem(
+                          value: todo,
+                          child: Text(todo.task),
+                        );
+                      }).toList(),
+                      hint: const Text("Select a Todo"),
+                      onChanged: (Todo? todo) {
+                        context.read<PomodoroBloc>().add(StartPomodoro(todo: todo));
+                      },
+                    ),
+                  ],
+                );
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          );
+        }
+      },
+    );
+  }
+
+  Column pomodoroCompleted(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        BlocBuilder<TodosStatusBloc, TodosStatusState>(
+          builder: (context, todosState) {
+            print('Todos state: $todosState');
+            if (todosState is TodosStatusLoaded) {
+              print('Pending todos count: ${todosState.pendingTodos.length}');
+              return Column(
+                children: [
+                  DropdownButton(
+                    items: todosState.pendingTodos.map((Todo todo) {
+                      return DropdownMenuItem(
+                        value: todo,
+                        child: Text(todo.task),
+                      );
+                    }).toList(),
+                    hint: const Text("Select a Todo"),
+                    onChanged: (Todo? todo) {
+                      context
+                          .read<PomodoroBloc>()
+                          .add(StartPomodoro(todo: todo));
+                    },
+                  ),
+                ],
+              );
+            } else {
+              return Center(child: const CircularProgressIndicator());
+            }
+          },
+        ),
+        Text(
+          'Pomodoro completed!',
+          style: Theme.of(context).textTheme.headline1,
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () => context.read<PomodoroBloc>().add(ResetPomodoro()),
+          child: const Text('Reset'),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () => context.read<PomodoroBloc>().add(StopPomodoro()),
+          child: const Text('Stop'),
+        ),
+      ],
+    );
+  }
+
+  Column pomodoroBreak(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        BlocBuilder<TodosStatusBloc, TodosStatusState>(
+          builder: (context, todosState) {
+            print('Todos state: $todosState');
+            if (todosState is TodosStatusLoaded) {
+              print('Pending todos count: ${todosState.pendingTodos.length}');
+              return Column(
+                children: [
+                  DropdownButton(
+                    items: todosState.pendingTodos.map((Todo todo) {
+                      return DropdownMenuItem(
+                        value: todo,
+                        child: Text(todo.task),
+                      );
+                    }).toList(),
+                    hint: const Text("Select a Todo"),
+                    onChanged: (Todo? todo) {
+                      context
+                          .read<PomodoroBloc>()
+                          .add(StartPomodoro(todo: todo));
+                    },
+                  ),
+                ],
+              );
+            } else {
+              return Center(child: const CircularProgressIndicator());
+            }
+          },
+        ),
+        Text(
+          'Take a break!',
+          style: Theme.of(context).textTheme.headline1,
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () => context.read<PomodoroBloc>().add(ResetPomodoro()),
+          child: const Text('Reset'),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () => context.read<PomodoroBloc>().add(StopPomodoro()),
+          child: const Text('Stop'),
+        ),
+      ],
+    );
+  }
+
+  Column pomodoroPaused(PomodoroPaused pomodoroState, BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        BlocBuilder<TodosStatusBloc, TodosStatusState>(
+          builder: (context, todosState) {
+            print('Todos state: $todosState');
+            if (todosState is TodosStatusLoaded) {
+              print('Pending todos count: ${todosState.pendingTodos.length}');
+              return Column(
+                children: [
+                  DropdownButton(
+                    items: todosState.pendingTodos.map((Todo todo) {
+                      return DropdownMenuItem(
+                        value: todo,
+                        child: Text(todo.task),
+                      );
+                    }).toList(),
+                    hint: const Text("Select a Todo"),
+                    onChanged: (Todo? todo) {
+                      context
+                          .read<PomodoroBloc>()
+                          .add(StartPomodoro(todo: todo));
+                    },
+                  ),
+                ],
+              );
+            } else {
+              return Center(child: const CircularProgressIndicator());
+            }
+          },
+        ),
+        Text(
+          '${(pomodoroState.secondsRemaining / 60).floor()}:${(pomodoroState.secondsRemaining % 60).toString().padLeft(2, '0')}',
+          style: Theme.of(context).textTheme.headline1,
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () => context.read<PomodoroBloc>().add(ResumePomodoro()),
+          child: const Text('Resume'),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () => context.read<PomodoroBloc>().add(ResetPomodoro()),
+          child: const Text('Reset'),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () => context.read<PomodoroBloc>().add(StopPomodoro()),
+          child: const Text('Stop'),
+        ),
+      ],
+    );
+  }
+
+  Column pomodoroRunning(PomodoroRunning pomodoroState, BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        BlocBuilder<TodosStatusBloc, TodosStatusState>(
+          builder: (context, todosState) {
+            print('Todos state: $todosState');
+            if (todosState is TodosStatusLoaded) {
+              print('Pending todos count: ${todosState.pendingTodos.length}');
+              return Column(
+                children: [
+                  DropdownButton(
+                    items: todosState.pendingTodos.map((Todo todo) {
+                      return DropdownMenuItem(
+                        value: todo,
+                        child: Text(todo.task),
+                      );
+                    }).toList(),
+                    hint: const Text("Select a Todo"),
+                    onChanged: (Todo? todo) {
+                      context
+                          .read<PomodoroBloc>()
+                          .add(StartPomodoro(todo: todo));
+                    },
+                  ),
+                ],
+              );
+            } else {
+              return Center(child: const CircularProgressIndicator());
+            }
+          },
+        ),
+        Text(
+          '${(pomodoroState.secondsRemaining / 60).floor()}:${(pomodoroState.secondsRemaining % 60).toString().padLeft(2, '0')}',
+          style: Theme.of(context).textTheme.headline1,
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () => context.read<PomodoroBloc>().add(PausePomodoro()),
+          child: const Text('Pause'),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () => context.read<PomodoroBloc>().add(ResetPomodoro()),
+          child: const Text('Reset'),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () => context.read<PomodoroBloc>().add(StopPomodoro()),
+          child: const Text('Stop'),
+        ),
+      ],
+    );
+  }
+
+  BlocBuilder<TodosStatusBloc, TodosStatusState> pomodoroBlocBuilder() {
+    return BlocBuilder<TodosStatusBloc, TodosStatusState>(
+      builder: (context, todosState) {
+        print('Todos state: $todosState');
+        if (todosState is TodosStatusLoaded) {
+          print('Pending todos count: ${todosState.pendingTodos.length}');
+          return Column(
+            children: [
+              DropdownButton(
+                items: todosState.pendingTodos.map((Todo todo) {
+                  return DropdownMenuItem(
+                    value: todo,
+                    child: Text(todo.task),
+                  );
+                }).toList(),
+                hint: const Text("Select a Todo"),
+                onChanged: (Todo? todo) {
+                  context.read<PomodoroBloc>().add(StartPomodoro(todo: todo));
+                },
+              ),
+            ],
+          );
+        } else {
+          return Center(child: const CircularProgressIndicator());
+        }
+      },
     );
   }
 }
