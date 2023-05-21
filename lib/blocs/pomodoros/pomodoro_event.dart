854e045b3ffccbb66307e721c9cefb00e8afca02
Isma-Akin <aakinsemoyin@hotmail.com>
2023-05-21 18:48:19 +0100

    refactor(pomodoro_event): add timerId and todoId attributes for events; add CreatePomodoro event

--- lib/blocs/pomodoros/pomodoro_event.dart
+++ lib/blocs/pomodoros/pomodoro_event.dart
@@ -2,23 +2,70 @@ part of 'pomodoro_bloc.dart';
 
 abstract class PomodoroEvent {}
 
+// class StartPomodoro extends PomodoroEvent {
+//   final Todo todo;
+//   final String timerId;
+//
+//   StartPomodoro(this.todo, {required this.timerId});
+//
+//   @override
+//   List<Object> get props => [timerId, todo];
+// }
 class StartPomodoro extends PomodoroEvent {
-  final Todo? todo;
+  final String timerId;
+  final String todoId;
 
-  StartPomodoro({required this.todo});
+  StartPomodoro({required this.timerId, required this.todoId});
+
+  List<Object> get props => [timerId, todoId];
 }
 
-class PausePomodoro extends PomodoroEvent {}
+class CreatePomodoro extends PomodoroEvent {
+  final String timerId;
+  final String todoId;
 
-class ResumePomodoro extends PomodoroEvent {}
+  CreatePomodoro({required this.timerId, required this.todoId});
+}
 
-class TickPomodoro extends PomodoroEvent {}
+class LoadPomodoro extends PomodoroEvent {
+  final String timerId;
+
+  LoadPomodoro({required this.timerId});
+}
+
+class PausePomodoro extends PomodoroEvent {
+  final String timerId;
+
+  PausePomodoro(this.timerId);
+}
+
+class ResumePomodoro extends PomodoroEvent {
+  final String timerId;
+
+  ResumePomodoro({required this.timerId});
+
+  List<Object> get props => [timerId];
+}
+
+class TickPomodoro extends PomodoroEvent {
+  final String timerId;
+
+  TickPomodoro(this.timerId);
+}
 
 class TickBreak extends PomodoroEvent {}
 
 class PomodoroCompleted extends PomodoroEvent {}
 
-class ResetPomodoro extends PomodoroEvent {}
+class ResetPomodoro extends PomodoroEvent {
+  final String timerId;
 
-class StopPomodoro extends PomodoroEvent {}
+  ResetPomodoro(this.timerId);
+}
+
+class StopPomodoro extends PomodoroEvent {
+  final String timerId;
+
+  StopPomodoro(this.timerId);
+}
 
