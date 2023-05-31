1102f9cf6eca72a483d26ead4e37d072aa8dd584
Isma-Akin <aakinsemoyin@hotmail.com>
2023-06-01 00:59:25 +0100

    refactor(pareto_models): add Todo to attributes, remove name attribute

--- lib/models/pareto_models.dart
+++ lib/models/pareto_models.dart
@@ -1,12 +1,17 @@
 import 'package:equatable/equatable.dart';
+import 'package:todoprocast_app/models/todo_models.dart';
 
 class ParetoItem extends Equatable {
   final String id;
-  final String name;
+  final Todo todo;
   final int value;
 
-  const ParetoItem({required this.id, required this.name, required this.value});
+  const ParetoItem({
+    required this.id,
+    required this.todo,
+    required this.value
+  });
 
   @override
-  List<Object> get props => [id, name, value];
+  List<Object> get props => [id, todo, value];
 }
