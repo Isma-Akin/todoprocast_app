e673ee2ace829c4e2b6c95421d931f3ca10d138f
Isma-Akin <aakinsemoyin@hotmail.com>
2023-06-01 01:01:16 +0100

    refactor(parkinsons_law_event): add timer Id; modify stop and tick events

--- lib/blocs/parkinsons_cubit/parkinsons_law_event.dart
+++ lib/blocs/parkinsons_cubit/parkinsons_law_event.dart
@@ -2,11 +2,21 @@ import '../../models/todo_models.dart';
 
 abstract class ParkinsonEvent {}
 class StartParkinson extends ParkinsonEvent {
+  final int timerId;
   final DateTime deadline;
   final Todo? todo;
-  StartParkinson(this.deadline, this.todo);
+  StartParkinson(this.timerId, this.deadline, this.todo);
 }
+
 class PauseParkinson extends ParkinsonEvent {}
 class ResumeParkinson extends ParkinsonEvent {}
-class StopParkinson extends ParkinsonEvent {}
-class TickParkinson extends ParkinsonEvent {}
+
+class StopParkinson extends ParkinsonEvent {
+  final int todoId;
+  StopParkinson(this.todoId);
+}
+
+class TickParkinson extends ParkinsonEvent {
+  final int todoId;
+  TickParkinson(this.todoId);
+}
