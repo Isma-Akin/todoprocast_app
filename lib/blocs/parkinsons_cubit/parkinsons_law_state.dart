68b0835ce464dedc7570d19bb932dbdab31f96b4
Isma-Akin <aakinsemoyin@hotmail.com>
2023-06-01 01:02:45 +0100

    refactor(parkinsons_law_state): modify completed and stopped states

--- lib/blocs/parkinsons_cubit/parkinsons_law_state.dart
+++ lib/blocs/parkinsons_cubit/parkinsons_law_state.dart
@@ -1,17 +1,39 @@
 abstract class ParkinsonState {}
 class ParkinsonInitial extends ParkinsonState {}
 
-class ParkinsonRunning extends ParkinsonState {
-  final int remainingTime;
-  ParkinsonRunning(this.remainingTime);
-}
+// class ParkinsonRunning extends ParkinsonState {
+//   final int todoId;
+//   final DateTime deadline;
+//
+//   ParkinsonRunning(this.todoId, this.deadline);
+// }
 
 class ParkinsonPaused extends ParkinsonState {
   final int remainingTime;
   ParkinsonPaused(this.remainingTime);
 }
-class ParkinsonCompleted extends ParkinsonState {}
+class ParkinsonCompleted extends ParkinsonState {
+  final String todoId;
+  ParkinsonCompleted(this.todoId);
+}
 
-class ParkinsonStopped extends ParkinsonState {}
+class ParkinsonStopped extends ParkinsonState {
+  final int todoId;
+  ParkinsonStopped(this.todoId);
+}
 
 class ParkinsonNotStarted extends ParkinsonState {}
+
+// class ParkinsonTicking extends ParkinsonState {
+//   final String todoId;
+//   final DateTime deadline;
+//
+//   ParkinsonTicking(this.todoId, this.deadline);
+// }
+
+class ParkinsonTicking extends ParkinsonState {
+  final int todoId;
+  final int remainingSeconds;
+
+  ParkinsonTicking(this.todoId, this.remainingSeconds);
+}
